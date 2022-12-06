d6ab159d3036169cc4a3859bfbe7e8cc5d5fb41f
Aleksandr Vasilyev <ap.vasilyev@yandex.ru>
2022-12-07 02:26:12 +0300

    refactoring: collection and stream tasks

--- src/main/java/tasks/Task6.java
+++ src/main/java/tasks/Task6.java
@@ -3,22 +3,34 @@ package tasks;
 import common.Area;
 import common.Person;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /*
 Имеются
 - коллекция персон Collection<Person>
 - словарь Map<Integer, Set<Integer>>, сопоставляющий каждой персоне множество id регионов
 - коллекция всех регионов Collection<Area>
-На выходе хочется получить множество строк вида "Имя - регион". Если у персон регионов несколько, таких строк так же будет несколько
+На выходе хочется получить множество строк вида "Имя - регион".
+Если у персон регионов несколько, таких строк так же будет несколько
  */
 public class Task6 {
 
   public static Set<String> getPersonDescriptions(Collection<Person> persons,
                                                   Map<Integer, Set<Integer>> personAreaIds,
                                                   Collection<Area> areas) {
-    return new HashSet<>();
+    Map<Integer, Area> idAreaMap = areas.stream()
+            .collect(Collectors.toMap(Area::getId, Function.identity()));
+    return persons.stream()
+            .flatMap(person -> personAreaIds.get(person.getId()).stream()
+                    .map(areaId -> createPersonDescription(person, idAreaMap.get(areaId))))
+            .collect(Collectors.toSet());
   }
+
+  private static String createPersonDescription(Person person, Area area) {
+      return person.getFirstName() + " - " + area.getName();
+  }
+
 }
